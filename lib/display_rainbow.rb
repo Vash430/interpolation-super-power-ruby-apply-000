0f44d764d256e6f8e48507f625e46b82f8fd8988
essential-form-0157 <vasmith430@gmail.com>
2020-07-10 20:11:41 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
-def display_rainbow(colors {R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet})
+def display_rainbow(colors {R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet} );
   puts "R: #{colors[0]}, 
         O: #{colors[1]}, 
         Y: #{colors[2]}, 
